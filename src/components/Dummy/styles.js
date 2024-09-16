32d82498b0fe729d64e9dc61b9ee59d69fd68841
Aruan Galves Amaral <aruangalves@gmail.com>
2024-09-16 16:58:29 -0300

    menuLink component

--- src/components/Dummy/styles.js
+++ src/components/Dummy/styles.js
@@ -1,5 +1,5 @@
 import styled, {css} from 'styled-components';
 
 export const Container = styled.div`
-  ${(/*{ props }*/) => css``};
+  ${(/* props */) => css``};
 `;
---
+++ src/components/MenuLink/styles.js
@@ -0,0 +1,28 @@
+import styled, {css} from 'styled-components';
+
+export const Container = styled.a`
+  ${( props ) => css`
+    display: block;
+    text-decoration: none;
+    font-size: ${props.theme.font.sizes.small};
+    padding: ${props.theme.spacings.gap};
+    color: ${props.theme.colors.primaryColor};
+    position: relative;
+
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 50%;
+      width: 0;
+      height: 0.2rem;
+      background: ${props.theme.colors.secondaryColor};
+      transition: left 300ms ease-in-out, width 300ms ease-in-out;
+    }
+
+    &:hover::after {
+      left: 25%;
+      width: 50%;
+    }
+  `};
+`;
