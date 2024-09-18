f3faa1b14f2c09c32445a1d2e056fa16259e168d
Aruan Galves Amaral <aruangalves@gmail.com>
2024-09-18 17:55:50 -0300

    Burger button for displaying/hiding menu in mobile mode

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,15 +3,29 @@ import * as Styled from './styles';
 //import { SectionContainer } from '../SectionContainer';
 import { LogoLink } from '../LogoLink';
 import { NavLinks } from '../NavLinks';
+import { Menu as MenuIcon } from '@styled-icons/material-outlined/Menu';
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
+import { useState } from 'react';
 
 export const Menu = ({ links = [], logoData}) =>{
+  const [menuVisible, setMenuVisible] = useState(false);
+
   return (
-    <Styled.Container >
-        <Styled.MenuContainer>
-          <LogoLink {...logoData} />
-          <NavLinks links={links} />
-        </Styled.MenuContainer>
-    </Styled.Container>
+    <>
+      <Styled.Button
+        visible={menuVisible}
+        onClick={() => setMenuVisible((v) => !v)}
+        aria-label="Open/close menu"
+      >
+        {menuVisible ? (<CloseIcon />) : (<MenuIcon />) }
+      </Styled.Button>
+      <Styled.Container visible={menuVisible} onClick={() => setMenuVisible(false)}>
+          <Styled.MenuContainer>
+            <LogoLink {...logoData} />
+            <NavLinks links={links} />
+          </Styled.MenuContainer>
+      </Styled.Container>
+    </>
   );
 };
 
--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,5 +1,10 @@
 import styled, {css} from 'styled-components';
 
+const menuVisible = () => css`
+  visibility: visible;
+  opacity: 1;
+`;
+
 export const Container = styled.header`
   ${( props ) => css`
     position: fixed;
@@ -11,6 +16,8 @@ export const Container = styled.header`
     border-bottom: ${props.theme.colors.mediumGray};
     padding: 0 3rem;
     background: ${props.theme.colors.lightBg};
+    transition: visibility 300ms ease-in-out;
+    transition: opacity 300ms ease-in-out;
 
     & H1{
       margin: 0 auto;
@@ -23,6 +30,12 @@ export const Container = styled.header`
       grid-template-rows: 1fr;
       align-items: center ;
       overflow-y: auto;
+
+      visibility: hidden;
+      opacity: 0;
+
+      ${props.visible && menuVisible()}
+
 
       & H1{
         padding-bottom: ${props.theme.spacings.gap};
@@ -48,5 +61,29 @@ export const MenuContainer = styled.header`
 `;
 
 export const Button = styled.button`
-  ${( /**props */ ) => css``}
+  ${( props ) => css`
+    z-index: 6;
+    position: fixed;
+    top: 2rem;
+    right: 2rem;
+    width: 4rem;
+    height: 4rem;
+    background: ${props.theme.colors.primaryColor};
+    color: ${props.theme.colors.lightBg};
+    border: none;
+    display: none;
+    pointer-events: ${props.visible ? 'none' : 'all'};
+    border-radius: 1rem;
+
+    @media ${props.theme.media.ltemedium} {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    >svg {
+      width: 2.4rem;
+      height: 2.4rem;
+    }
+  `}
 `;
