76e0c2ec9c53363a2e1b51065cd51adeabd26037
Aruan Galves Amaral <aruangalves@gmail.com>
2024-11-07 16:20:15 -0300

    Generating page from real API data

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
 import { Base } from '../Base';
-import mock from '../Base/mock';
+//import mock from '../Base/mock';
 import { mapData } from '../../liveapi/map-data';
 import { PageNotFound } from '../PageNotFound';
 import { PageLoading } from '../PageLoading';
+import { GridTwoColumns } from '../../components/GridTwoColumns';
+import { GridContent } from '../../components/GridContent';
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
 
 function App() {
 
@@ -46,9 +50,30 @@ function App() {
     return (<PageLoading />);
   }
 
+  const {title, menu, sections, footerText } = data;
+  const {imgSrc, link, links, text} = menu;
+
   return (
-    <Base {...mock} />
+    <Base links={links} footerHtml={footerText} logoData={{text, imgSrc, link}}>
+      <title>{title}</title>
+      {sections.map((section, index) => {
+        switch(section.component){
+          case 'section.section-two-columns':
+            return (<GridTwoColumns title={section.title} text={section.text} imgSrc={section.imgSrc} background={section.background} index={index} />);
+          case 'section.section-content':
+            return (<GridContent index={index} background={section.background} htmlcontent={section.htmlcontent} title={section.title} />);
+          case 'section.section-grid':
+            return (<GridText index={index} background={section.background} description={section.description} title={section.title} grid={section.grid} />);
+          case 'section.section-grid-gallery':
+            return (<GridImage index={index} background={section.background} description={section.description} title={section.title} grid={section.grid} />);
+        }
+      })}
+    </Base>
   )
+
+  /*return (
+    <Base {...mock} />
+  )*/
 }
 
 export default App
